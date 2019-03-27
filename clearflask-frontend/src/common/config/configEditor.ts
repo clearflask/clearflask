fa96f40c85029a1b65c0e2e23eeb873f4863a9e6
Matus Faro <matus@Matus-Lappy.local>
2019-03-27 18:14:25 +0800

    WIP Config editor.. it's going to take a while to get it right

--- clearflask-frontend/src/common/config/configEditor.ts
+++ clearflask-frontend/src/common/config/configEditor.ts
@@ -1,21 +1,62 @@
-import * as schema from '../../api/schema/schema-1.0.0.json';
+import * as Schema from '../../api/schema/schema-1.0.0.json';
 import { Config } from '../../api/admin/models/Config.js';
 
-// TODO
-// Use cases:
-// - Settings page
-// - In memory editing
+/**
+ * OpenApi vendor properties.
+ */
 
-interface Page {
-  path:ConfigPath;
-  name:string;
-  title:string;
+export enum OpenApiTags {
+  Page = 'x-clearflask-page', // Single page
+  PageGroup = 'x-clearflask-page-group', // Dynamic group of pages
+  Prop = 'x-clearflask-prop', // Property
+}
+export interface xCfPage {
+  name?:string;
+  order?:number;
+  title?:string;
   description?:string;
-  childPages?:Page[];
-  properties?:Property[];
+}
+export interface xCfPageGroup {
+  name?:string;
+  order?:number;
+  title?:string;
+  description?:string;
+}
+export interface xCfProp {
+  name?:string;
+  order?:number;
+  title?:string;
+  description?:string;
+  placeholder?:string;
+  defaultValue?:any;
 }
 
-type Property = 
+/**
+ * Settings objects
+ */
+
+export interface Page extends xCfPage {
+  type:'Page';
+  value?:any;
+  path:Path;
+  depth:ResolveDepth;
+  getPages():Page[];
+  getPageGroups():PageGroup[];
+  getProperties():Property[];
+}
+
+export interface PageGroup extends xCfPageGroup {
+  type:'PageGroup';
+  path:Path;
+  depth:ResolveDepth;
+  minItems?:number;
+  maxItems?:number;
+  getPages():Page[];
+  /** Use this method to create a new entry, then call groupPageInsert to commit. */
+  create:()=>Property;
+}
+
+export type Property =
   StringProperty
   |NumberProperty
   |IntegerProperty
@@ -23,93 +64,118 @@ type Property =
   |EnumProperty
   |ArrayProperty
   |ObjectProperty
-  |LinkProperty
-  |DateTimeProperty;
 
-interface PropertyBase {
+interface PropertyBase extends xCfProp{
   type:string;
-  value:any;
-  path:ConfigPath;
-  name:string;
-  description?:string;
+  value?:any;
+  defaultValue?:any;
+  path:Path;
   required:boolean;
 }
 
-interface StringProperty extends PropertyBase {
-  type:'StringProperty';
-  value:string;
-  valueType;
+export interface StringProperty extends PropertyBase {
+  type:'string';
+  value?:string;
+  defaultValue?:string;
+  minLength?:number;
+  maxLength?:number;
   validation?:RegExp;
+  format?:StringFormat|string;
 }
 
-interface NumberProperty extends PropertyBase {
-  type:'NumberProperty';
-  value:number;
+export interface NumberProperty extends PropertyBase {
+  type:'number';
+  value?:number;
+  defaultValue?:number;
+  multipleOf?:number;
 }
 
-interface IntegerProperty extends PropertyBase {
-  type:'IntegerProperty';
+export interface IntegerProperty extends PropertyBase {
+  type:'integer';
+  value?:number;
+  defaultValue?:number;
+  multipleOf?:number;
 }
 
-interface BooleanProperty extends PropertyBase {
-  type:'BooleanProperty';
-  value:boolean;
+export interface BooleanProperty extends PropertyBase {
+  type:'boolean';
+  value?:boolean;
+  defaultValue?:boolean;
 }
 
-interface EnumProperty extends PropertyBase {
-  type:'EnumProperty';
-  enum:string[];
+export interface EnumProperty extends PropertyBase {
+  type:'enum';
+  value?:string;
+  defaultValue?:string;
+  enumValues:string[];
+  enumNames:string[];
 }
 
-interface ArrayProperty extends PropertyBase {
-  type:'ArrayProperty';
-  itemProperty:Property;
-  // Since the Array type is unknown, use this method to
-  // create a new property and then use method propertyArrayInsert
-  // to actually insert it into the Array.
+export interface ArrayProperty extends PropertyBase {
+  type:'array';
+  value:Property[];
+  defaultValue?:void; // Use create instead
+  minItems?:number;
+  maxItems?:number;
+  uniqueItems?:boolean;
+  /** Use this method to create a new entry, then call propertyArrayInsert to commit. */
   create:()=>Property;
 }
 
-interface ObjectProperty extends PropertyBase {
-  type:'ObjectProperty';
-  value:void;
+export interface ObjectProperty extends PropertyBase {
+  type:'object';
+  value?:any;
+  defaultValue:void; // Use create instead
   properties:Property[];
+  create:()=>Property[];
 }
 
-interface LinkProperty extends PropertyBase {
-  type:'LinkProperty';
+/**
+ * Other
+ */
+
+export enum ResolveDepth {
+  None = 0, // Does not resolve pages nor properties
+  Shallow = 1, // Resolves only one level of pages and properties
+  Deep = 2, // Resolve all immediately
 }
 
-interface DateTimeProperty extends PropertyBase {
-  type:'DateTimeProperty';
+export enum StringFormat {
+  'Link' = 'link',
+  'Date' = 'date',
+  'DateTime' = 'date-time',
+  'Password' = 'password',
+  'Byte' = 'byte',
+  'Binary' = 'binary',
 }
 
-type ConfigPath = string[];
+/**
+ * Universal path used for both schema and config instance.
+ * String parts correspond to object values; numbers correpsond to array indices.
+ * 
+ * Ex: ['ideaSettings', 'tags', 3, 'color']
+ */
+export type Path = (string|number)[];
 
 interface ConfigEditor {
 
   clone():ConfigEditor;
   getConfig():Config;
 
-  listPages(path?:ConfigPath):Page[];
+  getPage(path:Path, depth?:ResolveDepth):Page;
+  getPageGroup(path:Path, depth?:ResolveDepth):PageGroup;
+  getProperty(path:Path):Property;
 
-  propertySet<P extends Property>(
-    property:P|ConfigPath,
-    value:P['value'])
-    :void;
-  propertyArrayInsert<P extends ArrayProperty>(
-    property:P|ConfigPath,
-    value:ReturnType<P['create']>,
-    index?:number)
-    :void;
-  propertyArrayDelete(
-    property:ArrayProperty|ConfigPath,
-    index:number)
-    :void;
+  propertySet<P extends Property>(prop:P, value:P['value']):void;
+  propertyArrayInsert<P extends ArrayProperty>(prop:P, valueProp:ReturnType<P['create']>, index?:number):void;
+  propertyArrayDelete(prop:ArrayProperty, index:number):void;
+  groupPageInsert(group:PageGroup, valuePage:Page, index?:number):void;
+  groupPageDelete(group:PageGroup, index:number):void;
 }
 
 export class ConfigEditorImpl implements ConfigEditor {
-  readonly config:Config;
+  config:Config;
+  cache:{[path: string]: Page|PageGroup|Property} = {};
 
   constructor(config:Config) {
     this.config = config;
@@ -121,31 +187,331 @@ export class ConfigEditorImpl implements ConfigEditor {
         JSON.stringify(
           this.config)));
   }
+
   getConfig():Config {
     return this.config;
   }
 
-  listPages(path?:ConfigPath):Page[] {
-    return []; // TODO
+  getPage(path:Path, depth:ResolveDepth = ResolveDepth.None, subSchema?:any):Page {
+    var result:Page|PageGroup|Property = this.getFromCache(path, () => this.parsePage(path, depth, subSchema), depth);
+    if(result.type !== 'Page') {
+      throw Error(`Expecting page type but found ${result.type} on path ${path}`);
+    }
+    return result;
   }
 
-  propertySet<P extends Property>(
-    property:P|ConfigPath,
-    value:P['value'])
-    :void {
-    // TODO
+  getPageGroup(path:Path, depth:ResolveDepth = ResolveDepth.None, subSchema?:any):PageGroup {
+    var result:Page|PageGroup|Property = this.getFromCache(path, () => this.parsePageGroup(path, depth, subSchema), depth);
+    if(result.type !== 'PageGroup') {
+      throw Error(`Expecting page group type but found ${result.type} on path ${path}`);
+    }
+    return result;
   }
-  propertyArrayInsert<P extends ArrayProperty>(
-    property:P|ConfigPath,
-    value:ReturnType<P['create']>,
-    index?:number)
-    :void {
-    // TODO
+
+  getProperty(path:Path, isRequired?:boolean, subSchema?:any):Property {
+    var result:Page|PageGroup|Property = this.getFromCache(path, () => this.parseProperty(path, isRequired, subSchema));
+    if(result.type === 'Page' || result.type === 'PageGroup') {
+      throw Error(`Expecting property type but found ${result.type} on path ${path}`);
+    }
+    return result;
   }
-  propertyArrayDelete(
-    property:ArrayProperty|ConfigPath,
-    index:number)
-    :void {
-    // TODO
+
+  getFromCache(path:Path, loader:()=>Page|PageGroup|Property, minDepth:ResolveDepth = ResolveDepth.None):Page|PageGroup|Property {
+    const key = this.getCacheKey(path);
+    var result:Page|PageGroup|Property = this.cache[key];
+    if(!result || ((result.type === 'Page' || result.type === 'PageGroup') && result.depth < depth)) {
+      result = loader();
+      this.cache[key] = result;
+    }
+    return result;
+  }
+
+  getValue(path:Path, subConfig?:any):any {
+    return path.reduce(
+      (subConfig, nextKey) => subConfig[nextKey]
+          || (() => {throw Error(`Cannot find ${subConfig && 'sub'}config value for key ${nextKey} in path ${path}`)})(),
+      subConfig || this.config);
+  }
+
+  setValue(path:Path, value:any):any {
+    if(path.length === 0) {
+      this.config = value;
+      return;
+    }
+    this.getValue(path.slice(0, -1))[path[path.length - 1]] = value;
+  }
+
+  propertySet<P extends Property>(prop:P, value:P['value']):void {
+    if(prop.required && value === undefined) {
+      throw Error(`Cannot assign undefined value to required property ${prop.path}`);
+    }
+    this.setValue(prop.path, value);
+    prop.value = value;
+  }
+
+  propertyArrayInsert<P extends ArrayProperty>(prop:P, valueProp:ReturnType<P['create']>, index?:number):void {
+    const arr = this.getValue(prop.path);
+    if(index) {
+      arr.splice(index, 0, valueProp.value);
+      prop.value.splice(index, 0, valueProp);
+    } else {
+      arr.push(valueProp.value);
+      prop.value.push(valueProp);
+    }
+  }
+
+  propertyArrayDelete(prop:ArrayProperty, index:number):void {
+    const arr = this.getValue(prop.path);
+    arr.splice(index, 1);
+    prop.value.splice(index, 1);
+  }
+
+  groupPageInsert(group:PageGroup, valuePage:Page, index?:number):void {
+    const arr = this.getValue(group.path);
+    if(index) {
+      arr.splice(index, 0, valuePage.value);
+      group.getPages().splice(index, 0, valuePage.value);
+    } else {
+      arr.push(valuePage.value);
+      group.getPages().push(valuePage);
+    }
+  }
+  groupPageDelete(group:PageGroup, index:number):void {
+    const arr = this.getValue(group.path);
+    arr.splice(index, 1);
+    group.getPages().splice(index, 1);
+  }
+
+  getCacheKey(path:Path):string {
+    return path.map(k => typeof k === 'string' ? k : '[]').join(':');
+  }
+
+  getSubSchema(path:Path, schema:any = Schema):any {
+    return path.reduce(
+      (subSchema, nextKey) =>
+        this.skipPaths(subSchema, ['allOf', 'properties'])
+          [typeof nextKey === 'number' ? 'items' : nextKey]
+            || (() => {throw Error(`Cannot find ${nextKey} in path ${path}`)})(),
+      schema);
+  }
+
+  skipPaths(schema:any, pathsToSkip:string[]):any {
+    pathsToSkip.forEach(pathToSkip => {
+      if(schema[pathToSkip]) {
+        schema = [pathToSkip]
+      };
+    });
+    return schema;
+  }
+
+  parsePage(path:Path, depth:ResolveDepth, subSchema?:any):Page {
+    const pageSchema = subSchema || this.getSubSchema(path);
+    const xPage = pageSchema[OpenApiTags.Page] as xCfPage;
+    if(!xPage) {
+      throw Error(`No page found on path ${path}`);
+    }
+
+    var getPages:()=>Page[];
+    var getPageGroups:()=>PageGroup[];
+    var getProperties:()=>Property[];
+
+    switch(depth) {
+      case ResolveDepth.Deep:
+      case ResolveDepth.Shallow:
+        const pages:Page[] = [];
+        const groups:PageGroup[] = [];
+        const props:Property[] = [];
+        const objSchema = this.skipPaths(pageSchema, ['allOf']);
+        const propsSchema = objSchema.properties
+          || (() => {throw Error(`Cannot find 'properties' under path ${path}`)})();
+        const requiredProps = objSchema.required || {};
+        Object.keys(propsSchema).forEach(propName => {
+          const propPath = [...path, propName];
+          const propSchema = this.getSubSchema([propName], propsSchema);
+          if(propSchema[OpenApiTags.Page]) {
+            pages.push(this.getPage(
+              propPath,
+              depth === ResolveDepth.Deep ? depth : ResolveDepth.None,
+              propSchema));
+          } else if(propSchema[OpenApiTags.PageGroup]) {
+            this.getValue(propPath).forEach(item => {
+              // TODO
+            });
+
+            const group:PageGroup = {
+              prop: { // TODO
+              },
+              page: this.getPage(
+                propPath,
+                depth === ResolveDepth.Deep ? depth : ResolveDepth.None,
+                propSchema),
+            };
+
+            // TODO
+            groups.push(group);
+          } else {
+            // TODO insert properties based on order. Here and in parseProperty too
+            props.push(this.getProperty(
+              propPath,
+              !!requiredProps[propName],
+              propsSchema));
+          }
+        });
+        getPages = () => pages;
+        getPageGroups = () => groups;
+        getProperties = () => props;
+        break;
+      default:
+      case ResolveDepth.None:
+        getPages = () => this.getPage(path, ResolveDepth.Shallow, pageSchema).getPages();
+        getPageGroups = () => this.getPage(path, ResolveDepth.Shallow, pageSchema).getPageGroups();
+        getProperties = () => this.getPage(path, ResolveDepth.Shallow, pageSchema).getProperties();
+      break;
+    }
+
+    const page:Page = {
+      ...xPage,
+      type: 'Page',
+      path: path,
+      depth: depth,
+      getPages: getPages,
+      getPageGroups: getPageGroups,
+      getProperties: getProperties,
+    };
+    return page;
+  }
+
+  parsePageGroup(path:Path, depth:ResolveDepth, subSchema?:any):PageGroup {
+    const pageGroupSchema = subSchema || this.getSubSchema(path);
+    const xPageGroup = pageGroupSchema[OpenApiTags.PageGroup] as xCfPageGroup;
+    if(!xPageGroup) {
+      throw Error(`No page group found on path ${path}`);
+    }
+
+    var getPages:()=>Page[];
+
+    switch(depth) {
+      case ResolveDepth.Deep:
+      case ResolveDepth.Shallow:
+        const count = this.getValue(path).length;
+        const pages:Page[] = [];
+        for(let i = 0; i < count; i++) {
+          pages.push(this.getPage([...path, i], depth, pageGroupSchema.items));
+        }
+        getPages = () => pages;
+        break;
+      default:
+      case ResolveDepth.None:
+        getPages = () => this.getPageGroup(path, ResolveDepth.Shallow, pageGroupSchema).getPages();
+      break;
+    }
+
+    const pageGroup:PageGroup = {
+      ...xPageGroup,
+      type: 'PageGroup',
+      path: path,
+      depth: depth,
+      minItems: pageGroupSchema.minItems,
+      maxItems: pageGroupSchema.maxItems,
+      getPages: getPages,
+    };
+    return pageGroup;
+  }
+
+  parseProperty(path:Path, isRequired?:boolean, subSchema?:any, valueOverride:any = null):Property {
+    if(path.length === 0) {
+      throw Error(`Property cannot be on root on path ${path}`);
+    }
+    var propSchema;
+    if(isRequired !== undefined && subSchema !== undefined) {
+      propSchema = subSchema;
+    } else {
+      const parentSchema = this.getSubSchema(path.slice(0, path.length - 1));
+      const propName = path[path.length - 1];
+      propSchema = parentSchema.properties && parentSchema.properties[propName]
+        || (() => {throw Error(`Cannot find property on path ${path}`)})();
+      isRequired = !!(parentSchema.required && parentSchema.required[propName]);
+    }
+    if(propSchema[OpenApiTags.Page]) {
+      throw Error(`Page found instead of property on path ${path}`);
+    }
+    const xProp = propSchema[OpenApiTags.Prop] as xCfProp;
+
+    const base:PropertyBase = {
+      ...xProp,
+      type: 'unknown',
+      path: path,
+      required: isRequired,
+    };
+    const value = valueOverride == null ? this.getValue(path) : valueOverride;
+    var property:Property;
+    switch(propSchema.type || 'object') {
+      case 'string':
+        property = {
+          ...base,
+          type: 'string',
+          value: value,
+          defaultValue: xProp.defaultValue || (isRequired ? '' : undefined),
+          minLength: propSchema.minLength,
+          maxLength: propSchema.maxLength,
+          validation: propSchema.pattern && new RegExp(propSchema.pattern),
+          format: propSchema.format,
+        };
+        break;
+      case 'number':
+      case 'integer':
+        property = {
+          ...base,
+          type: propSchema.type,
+          value: value,
+          defaultValue: xProp.defaultValue || (isRequired ? 0 : undefined),
+          multipleOf: propSchema.multipleOf,
+        };
+        break;
+      case 'boolean':
+        property = {
+          ...base,
+          type: 'boolean',
+          value: value,
+          defaultValue: xProp.defaultValue || (isRequired ? false : undefined),
+        };
+        break;
+      case 'array':
+        const create = () => {
+          // TODO
+        };
+        property = {
+          ...base,
+          type: 'array',
+          value: value,
+          defaultValue: xProp.defaultValue || (isRequired ? [] : undefined), // TODO fill in minimum number of items
+          minItems: propSchema.minItems,
+          maxItems: propSchema.maxItems,
+          uniqueItems: propSchema.uniqueItems,
+          create: create,
+        };
+        break;
+      case 'object':
+        const propsSchema = propSchema.properties
+          || (() => {throw Error(`Cannot find 'properties' under path ${path}`)})();
+        const requiredProps = propSchema.required || {};
+        const props:Property[] = Object.keys(propSchema.properties).map(propName =>
+          this.getProperty([...path, propName], !!requiredProps[propName], propsSchema));
+        const create = () => props.map(prop => {})
+          // TODO
+        ;
+        property = {
+          ...base,
+          type: 'object',
+          value: value,
+          defaultValue: xProp.defaultValue || (isRequired ? {} : undefined), // TODO fill in required keys
+          properties: props, // TODO
+          create: create,
+        };
+        break;
+      default:
+        throw Error(`Unknown type ${propSchema.type} in path ${path}`);
+    }
+    return property;
   }
 }
