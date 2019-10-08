f014af0ec37604d82ae50fcd181d18ea37d04fae
Matus Faro <matus@Matus-Lappy.local>
2019-10-08 17:37:50 +0800

    Renaming java classes from Veruv

--- clearflask-server/src/main/java/com/smotana/clearflask/core/ServiceInjector.java
+++ clearflask-server/src/main/java/com/smotana/clearflask/core/ServiceInjector.java
@@ -1,12 +1,8 @@
 package com.smotana.clearflask.core;
 
-import com.google.auth.Credentials;
-import com.google.cloud.NoCredentials;
-import com.google.cloud.storage.InMemoryStorageClient;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.util.concurrent.ServiceManager;
 import com.google.inject.*;
-import com.google.inject.Module;
 import com.google.inject.name.Names;
 import com.kik.config.ice.ConfigSystem;
 import com.kik.config.ice.convert.ConfigValueConverters;
@@ -15,34 +11,9 @@ import com.kik.config.ice.internal.ConfigDescriptorHolder;
 import com.kik.config.ice.naming.SimpleConfigNamingStrategy;
 import com.kik.config.ice.source.FileDynamicConfigSource;
 import com.kik.config.ice.source.JmxDynamicConfigSource;
-import com.smotana.clearflask.docker.BrowserCapabilitiesFactory;
-import com.smotana.clearflask.docker.DockerClientProvider;
-import com.smotana.clearflask.docker.DockerManagerImpl;
-import com.smotana.clearflask.gcloud.CachedMetadataConfig;
-import com.smotana.clearflask.image.ImageEditorImpl;
-import com.smotana.clearflask.image.VideoEncoderImpl;
-import com.smotana.clearflask.monitor.gcloud.ConsoleMetricsClient;
-import com.smotana.clearflask.monitor.gcloud.GCloudMetrics;
-import com.smotana.clearflask.monitor.gcloud.GCloudMetricsClientProvider;
-import com.smotana.clearflask.payment.MenuImpl;
-import com.smotana.clearflask.payment.StripePaymentImpl;
-import com.smotana.clearflask.resources.BrowserExpiry;
-import com.smotana.clearflask.resources.BrowserImpl;
-import com.smotana.clearflask.resources.BrowserResourceManagerImpl;
-import com.smotana.clearflask.resources.InMemoryResourceQueue;
-import com.smotana.clearflask.store.gcloud.DatastoreClientProvider;
-import com.smotana.clearflask.store.gcloud.GCloudCredentialsProvider;
-import com.smotana.clearflask.store.gcloud.GCloudStore;
-import com.smotana.clearflask.store.gcloud.StorageClientProvider;
 import com.smotana.clearflask.util.BeanUtil;
 import com.smotana.clearflask.util.GsonProvider;
-import com.smotana.clearflask.util.TimingAttackUtil;
-import com.smotana.clearflask.web.api.BrowserEndpoint;
-import com.smotana.clearflask.web.api.PingResource;
-import com.smotana.clearflask.web.api.ShutdownResource;
-import com.smotana.clearflask.web.api.VerificationResource;
-import com.smotana.clearflask.web.guard.*;
-import com.smotana.clearflask.web.guard.challenge.RecaptchaChallenger;
+import com.smotana.clearflask.web.api.AccountAdminResource;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -58,7 +29,7 @@ public enum ClearFlaskInjector {
     public enum Environment {
         UNIT_TEST(false),
         DEVELOPMENT_LOCAL(false),
-        PRODUCTION_GOOGLE_CLOUD(true);
+        PRODUCTION_AWS(true);
 
         private boolean isProduction;
 
@@ -75,7 +46,7 @@ public enum ClearFlaskInjector {
 
     public Injector get() {
         if (injector == null) {
-            synchronized (VeruvInjector.class) {
+            synchronized (ClearFlaskInjector.class) {
                 if (injector == null) {
                     Environment env = detectEnvironment();
                     log.info("Detected environment {}", env.name());
@@ -111,8 +82,6 @@ public enum ClearFlaskInjector {
                 install(ServiceManagerProvider.module());
                 install(GsonProvider.module());
 
-                // TODO add some modules here...
-
                 // Configuration
                 install(ConfigSystem.module());
                 install(SimpleConfigNamingStrategy.module());
@@ -127,33 +96,17 @@ public enum ClearFlaskInjector {
                 bind(Duration.class).annotatedWith(Names.named(FileDynamicConfigSource.POLL_INTERVAL_NAME)).toInstance(Duration.ofSeconds(10));
 
                 // API endpoints
-                install(BrowserEndpoint.module());
-                bind(PingResource.class);
-                bind(VerificationResource.class);
-                bind(ShutdownResource.class);
-                install(ShutdownResource.module());
-
-                // Common GCloud dependencies
-                install(GCloudStore.module());
-                install(CachedMetadataConfig.module());
-                install(DatastoreClientProvider.module());
-                install(GCloudMetrics.module());
+                bind(AccountAdminResource.class);
 
                 switch (env) {
                     case UNIT_TEST:
                     case DEVELOPMENT_LOCAL:
-                        bind(Credentials.class).toInstance(NoCredentials.getInstance());
-                        install(InMemoryStorageClient.module());
-                        install(ConsoleMetricsClient.module());
                         bind(String.class).annotatedWith(Names.named(FileDynamicConfigSource.FILENAME_NAME)).toInstance(
                                 getClass().getClassLoader().getResource("config-local.cfg").getPath());
                         break;
-                    case PRODUCTION_GOOGLE_CLOUD:
-                        install(GCloudCredentialsProvider.module());
-                        install(GCloudMetricsClientProvider.module());
-                        install(StorageClientProvider.module());
+                    case PRODUCTION_AWS:
                         bind(String.class).annotatedWith(Names.named(FileDynamicConfigSource.FILENAME_NAME)).toInstance(
-                                "/opt/veruv/config-prod.cfg");
+                                "/opt/clearflask/config-prod.cfg");
                         break;
                     default:
                         throw new RuntimeException("Unknown environment: " + env);
@@ -164,10 +117,10 @@ public enum ClearFlaskInjector {
     }
 
     private static Environment detectEnvironment() {
-        String envEnvironment = System.getenv("VERUV_ENVIRONMENT");
+        String envEnvironment = System.getenv("CLEARFLASK_ENVIRONMENT");
         if (envEnvironment != null) {
             return Environment.valueOf(envEnvironment);
         }
-        throw new RuntimeException("Could not determine environment. Did you forget to set env var VERUV_ENVIRONMENT?");
+        throw new RuntimeException("Could not determine environment. Did you forget to set env var CLEARFLASK_ENVIRONMENT?");
     }
 }
