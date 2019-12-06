95337ebfe6c23bad03f075094a2447791d21f74f
Matus Faro <matus@Matus-Lappy.local>
2019-12-06 11:17:10 +0800

    Class renaming

--- clearflask-server/src/main/java/com/smotana/clearflask/core/ServiceManagerProvider.java
+++ clearflask-server/src/main/java/com/smotana/clearflask/core/ServiceManagerProvider.java
@@ -1,36 +1,45 @@
 package com.smotana.clearflask.core;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Sets;
 import com.google.common.util.concurrent.Service;
 import com.google.common.util.concurrent.ServiceManager;
 import com.google.inject.AbstractModule;
 import com.google.inject.Inject;
 import com.google.inject.Module;
-import com.google.inject.Provider;
 import com.google.inject.Singleton;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.google.common.base.Preconditions.checkState;
 
 @Slf4j
-public class ServiceManagerProvider implements Provider<ServiceManager> {
-    @Inject
-    private Set<Service> services;
+@Singleton
+public class ManagedServiceManager {
+    private final ImmutableList<Service> allServices;
+    private final ServiceManager serviceManager;
 
-    @Override
-    @Singleton
-    public ServiceManager get() {
-        log.trace("Adding services to ServiceManager {}", services);
-        checkState(services != null, "Services are empty, make sure you inject Provider<ServiceManager> instead of injecting ServiceManager directly");
-        return new ServiceManager(services);
+    private ManagedServiceManager(@Inject Set<ManagedService> managedServices, @Inject Set<Service> services) {
+        checkState(Sets.intersection(services, managedServices).isEmpty());
+        allServices = ImmutableList.<Service>builder()
+                .addAll(services)
+                .addAll(managedServices.stream().sorted().collect(Collectors.toList()))
+                .build();
+        log.trace("Adding services to ServiceManager {}", allServices);
+        serviceManager = new ServiceManager((allServices));
+    }
+
+    public void startAndAwaitHealthy() {
+        allServices.forEach(Service::startAsync);
     }
 
     public static Module module() {
         return new AbstractModule() {
             @Override
             protected void configure() {
-                bind(ServiceManager.class).toProvider(ServiceManagerProvider.class).in(Singleton.class);
+                bind(ManagedServiceManager.class).asEagerSingleton();
             }
         };
     }
