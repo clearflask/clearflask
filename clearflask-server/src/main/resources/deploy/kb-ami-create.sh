8b9b953f24d3e90b6ebb24fc2457902ae4986c78
Matus Faro <matusfaro@gmail.com>
2020-10-02 03:58:05 +0800

    updated killbilll ami creation. ah finally deployed on aws

--- clearflask-server/src/main/resources/deploy/kb-ami-create.sh
+++ clearflask-server/src/main/resources/deploy/kb-ami-create.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-DB_PASSWORD=CC26FFAE-DA49-45B2-B6E9-53F7E655E426
-KAUI_API_SECRET=36ABD422-4401-41D5-AF8D-20ADE79478C0
-
 set -ex
 sudo yum install -y mc telnet
 sudo amazon-linux-extras install -y ruby2.6 tomcat8.5
@@ -11,7 +8,7 @@ sudo gem update --system
 sudo gem install io-console kpm
 sudo mkdir -p /var/lib/killbill
 sudo chown -R ec2-user:ec2-user /var/lib/killbill
-cat >/var/lib/killbill/kpm.yml <<"EOF"
+cat > /var/lib/killbill/kpm.yml <<"EOF"
 killbill:
   version: 0.22.10
   plugins:
@@ -30,62 +27,7 @@ kaui:
   webapp_path: /var/lib/tomcat/webapps/kaui/ROOT.war
 EOF
 sudo $(which kpm) install /var/lib/killbill/kpm.yml
-chown -R tomcat:tomcat /var/lib/tomcat/webapps
-
-sudo tee /var/lib/killbill/killbill.properties <<"EOF"
-org.killbill.billing.osgi.bundles.jruby.conf.dir=/var/lib/killbill/config
-org.killbill.billing.osgi.dao.password=$DB_PASSWORD
-org.killbill.billing.osgi.dao.url=jdbc:mysql://killbill.cluster-cobdzpzyvu7i.us-east-1.rds.amazonaws.com:3306/killbill
-org.killbill.billing.osgi.dao.user=killbill
-org.killbill.dao.password=$DB_PASSWORD
-org.killbill.dao.url=jdbc:mysql://killbill.cluster-cobdzpzyvu7i.us-east-1.rds.amazonaws.com:3306/killbill
-org.killbill.dao.user=killbill
-org.killbill.notificationq.analytics.historyTableName=analytics_notifications_history
-org.killbill.notificationq.analytics.tableName=analytics_notifications
-org.killbill.osgi.bundle.install.dir=/var/lib/killbill/bundles
-org.killbill.server.baseUrl=http://localhost:8080
-org.killbill.server.test.mode=false
-org.killbill.billing.plugin.kpm.kpmPath=/usr/local/bin/kpm
-org.killbill.billing.plugin.kpm.bundlesPath=/var/lib/killbill/bundles
-EOF
-
-sudo tee /etc/tomcat/conf.d/killbill.conf <<"EOF"
-JAVA_HOME=${JAVA_HOME-"/usr/lib/jvm/jre-1.8.0"}
-CATALINA_OPTS="-server
-               -showversion
-               -XX:+PrintCommandLineFlags
-               -XX:+UseCodeCacheFlushing
-               -Xms512m
-               -Xmx1024m
-               -XX:+CMSClassUnloadingEnabled
-               -XX:-OmitStackTraceInFastThrow
-               -XX:+UseParNewGC
-               -XX:+UseConcMarkSweepGC
-               -XX:+CMSConcurrentMTEnabled
-               -XX:+ScavengeBeforeFullGC
-               -XX:+CMSScavengeBeforeRemark
-               -XX:+CMSParallelRemarkEnabled
-               -XX:+UseCMSInitiatingOccupancyOnly
-               -XX:CMSInitiatingOccupancyFraction=50
-               -XX:NewSize=100m
-               -XX:MaxNewSize=256m
-               -XX:SurvivorRatio=10
-               -XX:+DisableExplicitGC"
-CATALINA_OPTS="$CATALINA_OPTS
-                      -Dkaui.api_key=clearflask
-                      -Dkaui.api_secret=$KAUI_API_SECRET
-                      -Dkaui.url=http://127.0.0.1:8081
-                      -Dkaui.db.url=jdbc:mysql://killbill.cluster-cobdzpzyvu7i.us-east-1.rds.amazonaws.com:3306/killbill
-                      -Dkaui.db.password=$DB_PASSWORD
-                      -Dkaui.db.username=killbill
-                      -Dkaui.root_username=admin
-                      -Dlogback.configurationFile=/var/lib/killbill/logback.xml
-                      -Dorg.killbill.server.properties=file:///var/lib/killbill/killbill.properties
-                      -Dcom.sun.xml.bind.v2.bytecode.ClassTailor.noOptimize=true
-                      -Djava.security.egd=file:/dev/./urandom
-                      -Djruby.compile.invokedynamic=false
-                      -Dlog4jdbc.sqltiming.error.threshold=1000"
-EOF
+sudo chown -R tomcat:tomcat /var/lib/tomcat/webapps
 
 sudo tee /etc/tomcat/server.xml <<"EOF"
 <?xml version='1.0' encoding='utf-8'?>
@@ -181,3 +123,63 @@ sudo tee /var/lib/killbill/logback.xml <<"EOF"
   </root>
 </configuration>
 EOF
+
+
+# THE BELOW FILES ARE ONLY A TEMPLATE
+# THEY CONTAIN PASSWORDS AND ARE LOCATED
+# IN killbill-secret S3 BUCKET
+DB_PASSWORD=this-is-not-the-right-password
+
+sudo tee /var/lib/killbill/killbill.properties <<"EOF"
+org.killbill.billing.osgi.bundles.jruby.conf.dir=/var/lib/killbill/config
+org.killbill.billing.osgi.dao.password=$DB_PASSWORD
+org.killbill.billing.osgi.dao.url=jdbc:mysql://killbill.cluster-cobdzpzyvu7i.us-east-1.rds.amazonaws.com:3306/killbill
+org.killbill.billing.osgi.dao.user=killbill
+org.killbill.dao.password=$DB_PASSWORD
+org.killbill.dao.url=jdbc:mysql://killbill.cluster-cobdzpzyvu7i.us-east-1.rds.amazonaws.com:3306/killbill
+org.killbill.dao.user=killbill
+org.killbill.notificationq.analytics.historyTableName=analytics_notifications_history
+org.killbill.notificationq.analytics.tableName=analytics_notifications
+org.killbill.osgi.bundle.install.dir=/var/lib/killbill/bundles
+org.killbill.server.baseUrl=http://localhost:8080
+org.killbill.server.test.mode=false
+org.killbill.billing.plugin.kpm.kpmPath=/usr/local/bin/kpm
+org.killbill.billing.plugin.kpm.bundlesPath=/var/lib/killbill/bundles
+EOF
+
+sudo tee /etc/tomcat/conf.d/killbill.conf <<"EOF"
+JAVA_HOME=${JAVA_HOME-"/usr/lib/jvm/jre-1.8.0"}
+CATALINA_OPTS="-server
+               -showversion
+               -XX:+PrintCommandLineFlags
+               -XX:+UseCodeCacheFlushing
+               -Xms512m
+               -Xmx1024m
+               -XX:+CMSClassUnloadingEnabled
+               -XX:-OmitStackTraceInFastThrow
+               -XX:+UseParNewGC
+               -XX:+UseConcMarkSweepGC
+               -XX:+CMSConcurrentMTEnabled
+               -XX:+ScavengeBeforeFullGC
+               -XX:+CMSScavengeBeforeRemark
+               -XX:+CMSParallelRemarkEnabled
+               -XX:+UseCMSInitiatingOccupancyOnly
+               -XX:CMSInitiatingOccupancyFraction=50
+               -XX:NewSize=100m
+               -XX:MaxNewSize=256m
+               -XX:SurvivorRatio=10
+               -XX:+DisableExplicitGC"
+CATALINA_OPTS="$CATALINA_OPTS
+                      -Dkaui.url=http://127.0.0.1:8080
+                      -Dkaui.db.url=jdbc:mysql://killbill.cluster-cobdzpzyvu7i.us-east-1.rds.amazonaws.com:3306/killbill
+                      -Dkaui.db.password=$DB_PASSWORD
+                      -Dkaui.db.username=killbill
+                      -Dkaui.root_username=admin
+                      -Dlogback.configurationFile=/var/lib/killbill/logback.xml
+                      -Dorg.killbill.server.properties=file:///var/lib/killbill/killbill.properties
+                      -Dcom.sun.xml.bind.v2.bytecode.ClassTailor.noOptimize=true
+                      -Djava.security.egd=file:/dev/./urandom
+                      -Djruby.compile.invokedynamic=false
+                      -Dlog4jdbc.sqltiming.error.threshold=1000"
+EOF
+
