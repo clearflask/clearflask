dba5f7430795146b953d78da1ae2a06e9247ddd2
Matus Faro <matusfaro@gmail.com>
2021-03-09 18:49:39 +0800

    WIP Commercial support

--- clearflask-frontend/src/common/util/debounce.ts
+++ clearflask-frontend/src/common/util/debounce.ts
@@ -4,7 +4,7 @@
 // be triggered. The function will be called after it stops being called for
 // N milliseconds. If `immediate` is passed, trigger the function on the
 // leading edge, instead of the trailing.
-export default function debounce<T extends any[]>(func: (...argary: T) => void, wait: number = SearchTypeDebounceTime, immediate: boolean = false): (...argary: T) => void {
+export default function debounce<T extends any[]>(func: (...argary: T) => void, wait: number = SearchTypeDebounceTime): (...argary: T) => void {
 	var timeout;
 	return function (...args: T) {
 		const later = () => {
