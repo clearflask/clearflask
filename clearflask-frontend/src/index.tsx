ffbdb950aeefe1233cd5d63c007467feed905cf0
Matus Faro <matusfaro@gmail.com>
2021-08-24 23:32:47 +0800

    index.tsx isn't called in SSR

--- clearflask-frontend/src/index.tsx
+++ clearflask-frontend/src/index.tsx
@@ -6,17 +6,14 @@ import { Integrations } from "@sentry/tracing";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { detectEnv, Environment, isProd } from './common/util/detectEnv';
-import windowIso from './common/windowIso';
 import Main from './Main';
 
-if (!windowIso.isSsr) {
-  Sentry.init({
-    dsn: "https://600460a790e34b3e884ebe25ed26944d@o934836.ingest.sentry.io/5884409",
-    integrations: [new Integrations.BrowserTracing()],
-    tracesSampleRate: !isProd() ? 1.0 : 0.1,
-    environment: detectEnv(),
-  });
-}
+Sentry.init({
+  dsn: "https://600460a790e34b3e884ebe25ed26944d@o934836.ingest.sentry.io/5884409",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: !isProd() ? 1.0 : 0.1,
+  environment: detectEnv(),
+});
 
 if (detectEnv() !== Environment.DEVELOPMENT_FRONTEND) {
   loadableReady(() => {
