ec8d58cae70c87dc9a1e5f37cad5610a848fa896
Matus Faro <matusfaro@gmail.com>
2021-12-05 23:08:54 +0800

    Another attempt at fixing auto language selection via CloudFront header

--- clearflask-frontend/src/i18n-csr.ts
+++ clearflask-frontend/src/i18n-csr.ts
@@ -3,18 +3,16 @@
 import i18n from 'i18next';
 import BrowserLanguageDetector from 'i18next-browser-languagedetector';
 import { htmlDataRetrieve } from './common/util/htmlData';
-import windowIso from './common/windowIso';
 import { getI18n as getI18nGeneric } from './i18n';
 
 var instance: typeof i18n | undefined;
 export const getI18n = () => {
   if (!instance) {
-    const initialLanguage = !windowIso.isSsr ? htmlDataRetrieve('__SSR_I18N_INIT_LNG__') : undefined;
     instance = getI18nGeneric(
-      initialLanguage,
-      htmlDataRetrieve('__SSR_I18N_INIT_STORE__'),
-      new BrowserLanguageDetector(),
+      i18n => i18n.use(BrowserLanguageDetector),
       {
+        lng: htmlDataRetrieve('__SSR_I18N_INIT_LNG__'),
+        resources: htmlDataRetrieve('__SSR_I18N_INIT_STORE__'),
         partialBundledLanguages: true,
       }
     );
--- clearflask-frontend/src/i18n-ssr.ts
+++ clearflask-frontend/src/i18n-ssr.ts
@@ -1,36 +1,30 @@
 // SPDX-FileCopyrightText: 2019-2021 Matus Faro <matus@smotana.com>
 // SPDX-License-Identifier: AGPL-3.0-only
 import CountryLocaleMap from 'country-locale-map';
-import i18nMiddleware, { LanguageDetector } from 'i18next-http-middleware';
+import i18nMiddleware from 'i18next-http-middleware';
 import { defaultLanguage, getI18n as getI18nGeneric, supportedLanguagesSet } from './i18n';
 
 export const getI18n = () => {
-  var languageDetector: LanguageDetector | undefined;
-  if (process.env.ENV === 'production') {
-    languageDetector = new i18nMiddleware.LanguageDetector();
-    languageDetector.addDetector({
-      name: 'by-header',
-      lookup: (req, res, options) => {
-        const headerCountry = req.header('x-clearflask-country-to-locale')?.toUpperCase();
-        console.info(`x-clearflask-country-to-locale: ${headerCountry}`);
-        if (!headerCountry) return defaultLanguage;
-        return CountryLocaleMap.getCountryByAlpha2(headerCountry)?.languages?.find(language =>
-          supportedLanguagesSet.has(language.toLowerCase()))
-          || defaultLanguage;
-      },
-    });
-  }
+  var languageDetector = new i18nMiddleware.LanguageDetector();
+  languageDetector.addDetector({
+    name: 'detection-via-cloudfront-header',
+    lookup: (req, res, options) => {
+      const headerCountry = req.header('x-clearflask-country-to-locale')?.toUpperCase();
+      console.info(`x-clearflask-country-to-locale: ${headerCountry}`);
+      if (!headerCountry) return defaultLanguage;
+      return CountryLocaleMap.getCountryByAlpha2(headerCountry)?.languages?.find(language =>
+        supportedLanguagesSet.has(language.toLowerCase()))
+        || defaultLanguage;
+    },
+    cacheUserLanguage: function (req, res, lng, options) {
+      // Don't store it
+    }
+  });
 
   return getI18nGeneric(
-    undefined,
-    undefined,
-    languageDetector,
+    i18n => i18n.use(languageDetector),
     {
       preload: [...supportedLanguagesSet],
-      ns: [
-        'app',
-        'site',
-      ],
     },
   );
 };
