faa8089721de65102d18c4d71dded7608c84d8ec
Matus Faro <matusfaro@users.noreply.github.com>
2022-03-27 12:44:40 +0000

    Re-testing BillingIT

--- clearflask-server/src/test/java/com/smotana/clearflask/web/resource/BillingIT.java
+++ clearflask-server/src/test/java/com/smotana/clearflask/web/resource/BillingIT.java
@@ -6,7 +6,6 @@ import com.google.common.collect.ImmutableList;
 import com.smotana.clearflask.api.model.AccountBilling;
 import com.smotana.clearflask.api.model.SubscriptionStatus;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -16,7 +15,7 @@ import static org.junit.Assert.assertEquals;
  *
  * Re-run when you make changes to Billing.
  */
-@Ignore
+//@Ignore
 @Slf4j
 public class BillingIT extends AbstractBlackboxIT {
 
