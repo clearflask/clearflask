6d30f5f6527cefb2592f00abace71ace562a5cbb
Matus Faro <matusfaro@gmail.com>
2022-04-29 16:01:17 +0800

    Fix escaping of SSR injected data

--- clearflask-frontend/src/common/util/htmlData.ts
+++ clearflask-frontend/src/common/util/htmlData.ts
@@ -1,9 +1,10 @@
 // SPDX-FileCopyrightText: 2019-2021 Matus Faro <matus@smotana.com>
 // SPDX-License-Identifier: Apache-2.0
+import he from 'he';
 import windowIso from '../windowIso';
 
 export function htmlDataCreate(id: string, data: any): string {
-  return `<script id="${id}" type="application/json">${JSON.stringify(data)}</script>`;
+  return `<script id="${id}" type="application/json" ver="2">${he.encode(JSON.stringify(data))}</script>`;
 }
 
 export function htmlDataRetrieve(id: string): any {
@@ -11,7 +12,8 @@ export function htmlDataRetrieve(id: string): any {
   const el = windowIso.document.getElementById(id);
   if (!el) return undefined;
   try {
-    return JSON.parse(el.innerHTML);
+    const content = el.getAttribute("ver") === "2" ? he.decode(el.innerHTML) : el.innerHTML;
+    return JSON.parse(content);
   } catch (e) {
     return undefined;
   }
