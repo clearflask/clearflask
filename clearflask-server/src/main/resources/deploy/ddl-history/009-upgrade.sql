2e7fdb1ecd4d59b529107720f853d5580ec92ac1
Matus Faro <matus@matus.io>
2023-11-15 23:16:32 -0500

    Missing use database

--- clearflask-server/src/main/resources/deploy/ddl-history/009-upgrade.sql
+++ clearflask-server/src/main/resources/deploy/ddl-history/009-upgrade.sql
@@ -1,6 +1,8 @@
  -- SPDX-FileCopyrightText: 2023 Matus Faro <matus@smotana.com>
  -- SPDX-License-Identifier: Apache-2.0
 
+USE killbill;
+
 -- Upgrade
 -- kpm migrations killbill killbill-0.22.20 killbill-0.22.32
 CREATE INDEX invoice_billing_events_tenant_account_record_id ON invoice_billing_events(tenant_record_id, account_record_id);
