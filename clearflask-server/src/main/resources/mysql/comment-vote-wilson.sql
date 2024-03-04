39ba1c0f36ae8924582af1dbd293b5fb45524233
Matus Faro <matus@matus.io>
2024-03-04 10:44:42 -0500

    Undo more desctructive code formatting on sql files causing issues

--- clearflask-server/src/main/resources/mysql/comment-vote-wilson.sql
+++ clearflask-server/src/main/resources/mysql/comment-vote-wilson.sql
@@ -1,55 +1,36 @@
 -- SPDX-FileCopyrightText: 2022-2022 Matus Faro <matussmotana.com>
 -- SPDX-License-Identifier: Apache-2.0
 
-DELIMITER
-//
+DELIMITER //
 
 CREATE FUNCTION vote_wilson(
-    upvotes BIGINT,
-    downvotes BIGINT,
-    z DOUBLE,
-    zSquared DOUBLE)
+        upvotes BIGINT,
+        downvotes BIGINT,
+        z DOUBLE,
+        zSquared DOUBLE)
     RETURNS DOUBLE DETERMINISTIC
-BEGIN
-        DECLARE
-numberOfSuccesses BIGINT;
-        DECLARE
-numberOfTrials BIGINT;
-        DECLARE
-zSquared DOUBLE;
-        DECLARE
-mean DOUBLE;
-        DECLARE
-factor DOUBLE;
-        DECLARE
-modifiedSuccessRatio DOUBLE;
-        DECLARE
-difference DOUBLE;
-        DECLARE
-lowerBound DOUBLE;
+    BEGIN
+        DECLARE numberOfSuccesses BIGINT;
+        DECLARE numberOfTrials BIGINT;
+        DECLARE zSquared DOUBLE;
+        DECLARE mean DOUBLE;
+        DECLARE factor DOUBLE;
+        DECLARE modifiedSuccessRatio DOUBLE;
+        DECLARE difference DOUBLE;
+        DECLARE lowerBound DOUBLE;
         -- See WilsonScoreInterval.java
-        SET
-numberOfSuccesses = IFNULL(upvotes, 0);
-        SET
-numberOfTrials = numberOfSuccesses + IFNULL(downvotes, 0);
-        IF
-numberOfTrials = 0 THEN
+        SET numberOfSuccesses = IFNULL(upvotes, 0);
+        SET numberOfTrials = numberOfSuccesses + IFNULL(downvotes, 0);
+        IF numberOfTrials = 0 THEN
             RETURN 0;
-END IF;
-        SET
-zSquared = z * z;
-        SET
-mean = numberOfSuccesses / numberOfTrials;
-        SET
-factor = 1 / (1 + (1 / numberOfTrials * zSquared));
-        SET
-modifiedSuccessRatio = mean + (1 / (2 * numberOfTrials) * zSquared);
-        SET
-difference = z * SQRT((1 / numberOfTrials * mean * (1 - mean)) + (1 / (4 * POW(numberOfTrials, 2)) * zSquared));
-        SET
-lowerBound = factor * (modifiedSuccessRatio - difference);
-RETURN lowerBound;
-END
-//
+        END IF;
+        SET zSquared = z * z;
+        SET mean = numberOfSuccesses / numberOfTrials;
+        SET factor = 1 / (1 + (1 / numberOfTrials * zSquared));
+        SET modifiedSuccessRatio = mean + (1 / (2 * numberOfTrials) * zSquared);
+        SET difference = z * SQRT((1 / numberOfTrials * mean * (1 - mean)) + (1 / (4 * POW(numberOfTrials, 2)) * zSquared));
+        SET lowerBound = factor * (modifiedSuccessRatio - difference);
+        RETURN lowerBound;
+    END //
 
 DELIMITER ;
