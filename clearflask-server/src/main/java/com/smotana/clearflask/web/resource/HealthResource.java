3cd2cdd8cbfae2c4afccfb230b6e9890c8cb7b74
Matus Faro <matus@matus.io>
2024-10-11 09:48:22 -0400

    Endpoint to check version of ClearFlask

--- clearflask-server/src/main/java/com/smotana/clearflask/web/resource/HealthResource.java
+++ clearflask-server/src/main/java/com/smotana/clearflask/web/resource/HealthResource.java
@@ -2,6 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.smotana.clearflask.web.resource;
 
+import com.google.common.base.Charsets;
+import com.google.common.base.Strings;
+import com.google.common.io.Resources;
 import com.google.common.util.concurrent.ServiceManager;
 import com.google.inject.AbstractModule;
 import com.google.inject.Module;
@@ -13,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
@@ -22,6 +26,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Optional;
 
 @Slf4j
 @Singleton
@@ -45,6 +50,32 @@ public class HealthResource {
             throw new ApiException(Response.Status.SERVICE_UNAVAILABLE);
         }
         return "ok";
+    }
+
+    @GET
+    @Path("version")
+    @Consumes(MediaType.WILDCARD)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String version(@Context ServletContext servletContext) {
+        String gitPropertiesStr = Optional.ofNullable(Thread.currentThread().getContextClassLoader().getResource("git.properties"))
+                .map(url -> Resources.toString(url, Charsets.UTF_8))
+                .orElse("Cannot find git.properties");
+
+        String contextVersionStr = Optional.ofNullable(servletContext.getRealPath("/"))
+                // Typical value: "/var/lib/tomcat/webapps/150clearflask/ROOT##202410110929-1.7.11-SNAPSHOT/"
+                .map(path -> path.replaceAll(".*##([^/]+).*", "$1"))
+                .map(Strings::emptyToNull)
+                .orElse("Cannot find context version");
+
+        return gitPropertiesStr + "\n" + contextVersionStr;
+    }
+
+    @GET
+    @Path("contextPath")
+    @Consumes(MediaType.WILDCARD)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String contextPath(@Context ServletContext servletContext) {
+        return Optional.ofNullable(servletContext.getContextPath()).toString();
     }
 
     public static Module module() {
